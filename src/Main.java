670c4714610f3f29caf2419ca4238be55ac56cc4
somyaraj <somyarajbatham@gmail.com>
2024-04-26 14:35:48 +0530

    Git Desktop
    UI and Functionality changes for calculator

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import com.manishaPharma.calculato.constants.PxConstants;
+import com.manishaPharma.calculator.ui.CalculatorUI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CalculatorUI calculatorUI = new CalculatorUI(PxConstants.TITLE);
+    }
+}
---
+++ src/com/manishaPharma/calculato/constants/PxConstants.java
@@ -0,0 +1,42 @@
+package com.manishaPharma.calculato.constants;
+
+public class PxConstants {
+
+    public static final String TITLE = "Manisha Pharma";
+
+    public static final String PTR_PTS_TITLE = "PTR and PTS Calculator";
+
+    public static final String MRP = "MRP";
+
+    public static final String GST = "GST %";
+
+    public static final String RETAIL_MARGIN = "Retail Margin %";
+
+    public static final String STOCKIST_MARGIN = "Stockist margin %";
+
+    public static final String PTR = "PTR";
+
+    public static final String PTS = "PTS";
+
+    public static final String SCHEME = "Scheme %";
+
+    public static final String NET_SCHEME = "Net Scheme";
+
+    public static final String ENTER_BUTTON_LABEL = "Enter";
+
+    public static final String CLEAR_BUTTON_LABEL = "Clear";
+
+    public static final String FONT_NAME = "Arial";
+
+    public static final int TEXT_FIELD_SIZE = 15;
+
+    public static final int FRAME_WIDTH = 400;
+
+    public static final int FRAME_HEIGHT = 350;
+
+    public static final int LABEL_HEADER_SIZE = 20;
+
+    public static final String EMPTY_STRING = "";
+
+    public static final String ERROR_EMPTY_STRING = " cannot be Empty!";
+}
